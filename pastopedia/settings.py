2c4ab3cc18344f34a7d7d639a24b308776d315dd
BarenyaKumar <barenyakumar@gmail.com>
2022-02-17 23:50:29 +0530

    added links

--- pastopedia/settings.py
+++ pastopedia/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1my+77u^mn9u7@&tcr@@l9=27(uazf5txa%78bn(e#1$$=oel4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dustbin4devs.herokuapp.com','127.0.0.1:8000']
 
